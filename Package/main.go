d3344d774a0c296e3eb5deca19cce2f854fff603
Esteban Garriga <egarriga@flp.uy>
2026-08-17 11:03:43 -0300

    Initial commit

---
+++ Package/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	envVar := os.Getenv("HOME")
+	if envVar == "" {
+		fmt.Println("The HOME environment variable is not set.")
+	} else {
+		fmt.Printf("The HOME environment variable is set to: %s\n", envVar)
+	}
+
+	file, err := os.Create("example.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+}
---
+++ Variables/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+	
+func main(){
+	var cadena string= "Inicial"
+	fmt.Println(cadena)
+
+	var entero1, entero2 int = 10, 20
+	fmt.Println(entero1, entero2)
+
+	var booleano = true
+	fmt.Println(booleano)
+	
+	var enteroSimpre int 
+	fmt.Println(enteroSimpre)
+	enteroSimpre = 100
+	fmt.Println(enteroSimpre)
+
+	fruta := "Manzana"
+	fmt.Println(fruta)
+	 
+
+}
