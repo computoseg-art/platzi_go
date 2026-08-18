ba39123d1d942943fa920bb2f79340ed9335397f
Esteban Garriga <egarriga@flp.uy>
2026-08-18 04:35:50 -0300

    carpeta Condiciones

--- Condiciones/main.go
+++ Condiciones/main.go
@@ -1,1 +1,28 @@
-package condiciones
+package main
+
+import "fmt"
+
+func main() {
+	nombre := "Admin"
+	edad := 40
+	
+	if nombre == "Admin" && edad >= 18 {
+		fmt.Println("Acceso permitido")
+	} else {
+		fmt.Println("Acceso denegado")
+	}
+
+	if 8%2 == 0 {
+		fmt.Println("El número 8 es par")
+	}else{
+		fmt.Println("El número 8 es impar")
+	}
+
+	if numero :=9; numero > 0 {
+		fmt.Println("El número es positivo")
+	}else if numero < 0 {
+		fmt.Println("El número es negativo")
+	}else{
+		fmt.Println("El número es cero")
+	}
+}
